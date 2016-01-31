52cd0b968f552c0d8de052ba87a245a5fc3a83ca
Aidan Swope <Aidan Swope>
2016-01-30 18:55:12 -1000

    Added and tuned fear of toothy volcanoes. This is the best commit message I've ever written.

---
+++ over-the-edge/Assets/Scripts/VolcanoFear.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+private final var rangeOfInfluence = 200;
+private final var forceMagnitude = -250; //OH GOD IT'S A VOLCANO WITH TEETH
+private final var forceDistanceExponent = -1; //Well, at a distance it's not so bad...
+
+function Start () {
+
+}
+
+function Update () {
+	var people : GameObject[];
+	people = GameObject.FindGameObjectsWithTag("SwarmAgent"); 
+	var withinRange = 0;
+	for (var person in people) {
+		var deltaX = this.gameObject.transform.position.x - person.transform.position.x;
+		var deltaZ = this.gameObject.transform.position.z - person.transform.position.z;
+		var angle = Mathf.Atan2(deltaZ, deltaX);
+		var rigidbody : Rigidbody = person.GetComponent("Rigidbody");
+		var distance = Mathf.Sqrt(Mathf.Pow(deltaX, 2) + Mathf.Pow(deltaZ, 2));
+		var distanceMultiplier = Mathf.Pow(distance, forceDistanceExponent);
+		var forceVector : Vector3 = Vector3(Mathf.Cos(angle) * forceMagnitude * distanceMultiplier, 0, Mathf.Sin(angle) * forceMagnitude * distanceMultiplier);
+    	if (distance <= rangeOfInfluence) {
+    		rigidbody.AddForce(forceVector);
+    		withinRange++;
+    	}
+	}
+}
