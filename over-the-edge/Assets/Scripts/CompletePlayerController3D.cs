f0c150f94f61862e497422eb2ef44ac1ca802124
Aidan Swope <Aidan Swope>
2016-01-30 16:06:07 -1000

    Cleaned up player controller script

--- over-the-edge/Assets/Scripts/CompletePlayerController3D.cs
+++ over-the-edge/Assets/Scripts/CompletePlayerController3D.cs
@@ -10,11 +10,8 @@ public class CompletePlayerController3D : MonoBehaviour
 {
 
     public float maxSpeed;             //Floating point variable to store the player's movement speed.
-    public Text countText;          //Store a reference to the UI Text component which will display the number of pickups collected.
-    public Text winText;            //Store a reference to the UI Text component which will display the 'You win' message.
-
+    
     private Rigidbody rb3d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
-    private int count;              //Integer to store the number of pickups collected so far.
 
     // Use this for initialization
     void Start()
@@ -34,17 +31,6 @@ public class CompletePlayerController3D : MonoBehaviour
         {
             rb3d.AddForce(-rb3d.velocity * (rb3d.velocity.magnitude - maxSpeed));
         }
-        //Store the current horizontal input in the float moveHorizontal.
-        //float moveHorizontal = Input.GetAxis ("Horizontal");
-
-        //Store the current vertical input in the float moveVertical.
-        //float moveVertical = Input.GetAxis ("Vertical");
-
-        //Use the two store floats to create a new Vector2 variable movement.
-        //Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-
-        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        //rb2d.AddForce (movement * speed);
     }
 
 
