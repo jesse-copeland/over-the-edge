d7392de3adb6e3f87845ba6b2cc09a30477f3291
Aidan Swope <Aidan Swope>
2016-01-30 18:25:08 -1000

    Implemented omen reserve and cooldown system

--- over-the-edge/Assets/Scripts/UpdateMouse.js
+++ over-the-edge/Assets/Scripts/UpdateMouse.js
@@ -11,6 +11,8 @@ var maxMouthDepth = 255; //Maximum raycast distance
 var prefabTotem : GameObject;
 var prefabOmen : GameObject;
 
+private final var approximateFrameRate = 50;
+
 // Mouse button mapping //
 private final var mouseButtonLeft = 0;
 private final var mouseButtonRight = 1;
@@ -19,11 +21,34 @@ private final var mouseButtonMiddle = 2;
 // Constants //
 private final var spawnHeight = 5;
 
+// Omen spawning stuff //
+private var omenReserves = 3; //How many omens does the player have to spend?
+private final var maxOmenReserves = 5; //How many omens can a player hold in reserve?
+private final var maxSimultaneousOmens = 3; //How many omens can the player have active at once?
+private final var secondsToGainOmen = 15;
+private final var framesToGainOmen = secondsToGainOmen * approximateFrameRate;
+private var omenGainTimer = framesToGainOmen;
+private final var secondsToCooldownOmen = 1;
+private final var framesToCooldownOmen = secondsToCooldownOmen * approximateFrameRate;
+private var omenCooldown = 0;
+
 function Start () {
 
 }
 
 function Update () {
+	// Handle omen gain stuff //
+	// Omen gain timer //
+	if (omenGainTimer > 0) {
+		omenGainTimer--;
+	} else if (omenReserves < maxOmenReserves) {
+		omenReserves++;
+		omenGainTimer = framesToGainOmen;
+	}
+
+	// Omen cooldown timer //
+	if (omenCooldown > 0) omenCooldown--;
+
 	if (Input.GetMouseButtonDown(mouseButtonLeft)) mousePressedLeft();
 	if (Input.GetMouseButtonDown(mouseButtonRight)) mousePressedRight();
 	if (Input.GetMouseButtonDown(mouseButtonMiddle)) mousePressedMiddle();
@@ -62,7 +87,7 @@ function mousePressedLeft() {
 }
 
 function mousePressedRight() {
-	// Things that happen when the mouse is clicked will go here //
+	// Things that happen when the mouse is right-clicked will go here //
 	var clickPosition : Vector3 = getMouseVector();
 	var clickedObject : GameObject = getObjectClicked();
 
@@ -72,6 +97,12 @@ function mousePressedRight() {
 	var x = clickPosition.x;
 	var y = spawnHeight;
 	var z = clickPosition.z;
+
+	// Make sure we have enough omens //
+	if (omenReserves <= 0 || omenCooldown > 0) return;
+
+	omenReserves--;
+	omenCooldown = framesToCooldownOmen;
 	Instantiate(prefabOmen, Vector3 (x, y, z), Quaternion.identity);
 }
 
